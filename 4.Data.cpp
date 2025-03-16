aef5736aa8e426bf7782a7814a60baa213c8419b
winnerpooh8 <winnerpooh8@gmail.com>
2025-03-16 11:59:21 +0900

    2025-03-16

---
+++ 4.Data.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+	int a = 1;
+	float  f = 1.5f; //소수점6자리까지표현
+	double  d = 1.5; //소수점15자리까지표현
+	bool isDead = false;
+	string name = "초록매실";
+	return 0;
+}
+
