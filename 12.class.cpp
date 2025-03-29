fd54c68f72ede22a7ad4e4dd6234dbf300d27d64
winnerpooh8 <winnerpooh8@gmail.com>
2025-03-29 11:54:48 +0900

    2025-03-29

---
+++ 12.class.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+//c++클래스
+//클래스 : 데이터와 함수를 하나로 묶은 사용자 정의 데이터 타입
+//클래스는 프로그램 '객체'를 만들기 위한 설계도 역할을 합니다.
+
+//ex) ATM에 대한 클래스 설계
+//ATM에 필요한 핵심 데이터 나열
+//1. 사용자(ATM을 이용합니다.)
+//2. ATM 기기(담당 관리자, 위치)
+//3. 은행(ATM이 연결할 대상) (은행의 위치)
+//4. 계좌(사용자가 은행에 만든) (잔액,기능(예금,출금,입금)
+//5. 예금
+//6.  
+
+//클래스 설계의 기본 법칙
+//private를 통해, 직접적으로 접근하면 안되는 정보들을 설계하고,
+//public을 통해 외부에 기능을 전달합니다.
+
+//좌석 클래스 만들기
+class Seat
+{
+//private에 적혀있는 변수 또는 함수는 공개되지 않습니다.(외부에서 접근할 수 없습니다.)
+private :
+	string name;
+//public에 적혀있는 변수 또는 함수는 공개됩니다.(외부에서 사용할 수 있습니다.)
+public : 
+	//생성자는 클래스와 이름이 동일한 함수를 의미합니다.
+	//클래스 형태의 객체(데이터)가 생성되었을 때, 호출됩니다.
+	Seat()
+	{
+		name = "{---}";
+	}
+	void reset()
+	{
+		name = "{---}";
+	}
+	void setName(string name)
+	{
+		//this ->name은 현재 클래스 Seat 자신이 가지고 name임을 표현합니다.
+		this-> name = name; 
+	}
+	string getName()
+	{
+		return name;
+	}
+
+};
+
+
+
+int main()
+{
+	Seat* seats = new Seat[5];
+	cout << "김준표님께서 2번 자리에 예약하셨습니다." << endl;
+	seats[1].setName("김준표");
+	for (int i = 0; i < 5; i++)
+	{
+		cout << seats[i].getName() << "";
+	}
+
+	return 0;
+}
+
